a2344f55777c2f899b01ccc1e23a9aec15db6db1
sphinx-austin <austinjuniour4@gmail.com>
2024-04-29 16:12:34 +0300

    instructor embeddings

--- app.py
+++ app.py
@@ -1,11 +1,45 @@
 import streamlit as st
 from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_community.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+# from langchain-community.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+from langchain.vectorstores import FAISS
+
+
+
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+def get_vectorstore(text_chunks):
+    # embeddings = OpenAIEmbeddings()
+    embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vectorstore
+
 
 
 
 def main():
     load_dotenv()
-    
+
 
 
     st.set_page_config(page_title="Chat with multiple PDFs", page_icon=":books:")
@@ -15,8 +49,21 @@ def main():
 
     with st.sidebar:
         st.subheader("Your documents list")
-        st.file_uploader("Upload PDFs and click on 'Process' ", type="pdf", accept_multiple_files=True)
-        st.button("Process")
+        pdf_docs = st.file_uploader("Upload PDFs and click on 'Process' ", type="pdf", accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Processing..."):
+
+                # get pdf text
+                raw_text = get_pdf_text(pdf_docs)
+                # st.write(raw_text)
+
+                # get text chunk
+                text_chunks = get_text_chunks(raw_text)
+                # st.write(text_chunks)
+
+                # create vectorizer store
+                vectorstore = get_vectorstore(text_chunks)
+
 
 
 
